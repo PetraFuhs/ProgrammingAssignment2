2ed84e5be113d83333bcac50ba8c584840c73a8b
PetraFuhs <PetraFuhs@users.noreply.github.com>
2015-10-24 23:04:33 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,59 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        # Let x be a square and invertible matrix. (Otherwise R will throw an error).
+        # makeCacheMatrix returns a list as input for cacheSolve.
+        
+        # "inv": inverted matrix 
+        inv = NULL
+        
+        # "set" puts y to x outside of the current environment.
+        set = function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        # "get" returns matrix
+        get = function() x
+        
+        # "setinv" puts inv outside of the current environment
+        setinv = function(inverse) inv <<- inverse 
+        
+        # "getinv" returns inv
+        getinv = function() inv
+        
+        # Returning the list:
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
+# ------------
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        # Computes the inverse of the special "matrix" 
+        # returned by makeCacheMatrix above. 
+        # If the inverse has already been calculated 
+        # (and the matrix has not changed), then the cacheSolve 
+        # retrieves the inverse from the cache.
+        
+        # x is the output list of makeCacheMatrix()
+        
+        # get inv
+        inv = x$getinv()
+        
+        # if inv is not NULL (if calculated before): get it from the cache and returns it
+        if (!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        
+        # if inv still is NULL (not calculated by now): the inverse is calculated
+        data = x$get()
+        inv = solve(data, ...)
+        
+        # the calculated inverse is stored
+        x$setinv(inv)
+        
+        # ready!
+        return(inv)
 }
+
